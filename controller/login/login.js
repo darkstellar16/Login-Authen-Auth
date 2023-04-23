71b848b6732fab3f80907c50f02d6e7ffc5f2f02
darkstellar16 <itsmeshubham2799@gmail.com>
2023-04-24 00:51:59 +0530

    created login route and added login page

--- controller/login/login.js
+++ controller/login/login.js
@@ -1,5 +1,7 @@
 const loginInfo = require("../../model/userModel.js");
 
+const jwt = require("jsonwebtoken");
+
 const bcrypt = require("bcrypt");
 
 const loginData = async (req, res) => {
@@ -7,33 +9,20 @@ const loginData = async (req, res) => {
 
         const { email, password } = req.body;
         if (!(email && password)) {
-            res.status(404).send("Fields required");
+            return res.status(404).send("Fields required");
         }
         const user = await loginInfo.findOne({ email });
         if (user && (await bcrypt.compare(password, user.password))) {
             const token = jwt.sign({ email, password }, "fuck off to this life", { expiresIn: "2h" })
 
             user.token = token;
+            res.status(200).json(user);
         }
-
-        
+        res.status(400).send("invalid credentials")
 
     } catch (error) {
+        console.log(error);
 
     }
-
-
-
-
 }
-
-
-
-
-
-
-
-
-
-
 module.exports = loginData;
